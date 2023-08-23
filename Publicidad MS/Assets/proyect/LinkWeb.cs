8f014c8d6d9e7809765cef52dceb04e6f8ab0c0d
Perpendicular_Cow <andresarngvl@gmail.com>
2023-08-22 23:20:26 -0500

    creo que funciono

---
+++ Publicidad MS/Assets/proyect/LinkWeb.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkWeb : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+}
---
+++ Publicidad MS/Assets/proyect/linkvideo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linkvideo : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+
+}
